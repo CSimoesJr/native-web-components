7f8979eddb488043f86f4355ccb8cfa6e206b8a9
CSimoesJr <ju_csimoes@hotmail.com>
2023-06-07 17:14:00 -0300

    Renderizando html de react web

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import { WebView } from 'react-native-webview';
-const myHtmlFile = require('./build/index.html')
+import WebView from 'react-native-webview';
+
 export default function App() {
-  return <WebView source={myHtmlFile} style={{ flex: 1 }} />;
+  // return <RenderHtml contentWidth={width} source={source} />;
+  const PolicyHTML = require('./build/index.html');
+  return (
+    // <WebView
+    //   originWhitelist={['*']}
+    //   source={{uri: 'file:///android_asset/build/index.html'}}
+    //   style={{flex: 1}}
+    //   javaScriptEnabled={true}
+    //   domStorageEnabled={true}
+    // />
+
+    <WebView
+      source={{uri: 'http://localhost:3000/'}}
+      style={{flex: 1}}
+    />
+  );
 }
