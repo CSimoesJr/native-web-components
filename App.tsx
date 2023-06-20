5720bc46c7f6ef26eb4bff1f717b92449b7fc0cd
CSimoesJr <ju_csimoes@hotmail.com>
2023-06-20 00:15:50 -0300

    criando html template com angular element

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import WebView from 'react-native-webview';
 
 export default function App() {
   // return <RenderHtml contentWidth={width} source={source} />;
-  const PolicyHTML = require('./build/index.html');
   return (
     // <WebView
     //   originWhitelist={['*']}
@@ -13,9 +12,42 @@ export default function App() {
     //   domStorageEnabled={true}
     // />
 
+    // <WebView
+    //   source={{uri: 'http://localhost:3000/'}}
+    //   style={{flex: 1}}
+    // />
     <WebView
-      source={{uri: 'http://localhost:3000/'}}
-      style={{flex: 1}}
+      source={{
+        html: ` 
+        <html lang="en">
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <script src="file:///android_asset/build/angular-element.js"></script>
+          <link rel="stylesheet" href="file:///android_asset/build/styles.css" />
+        </head>
+        <body>
+          <div id="root"></div>
+          <div className="App">
+            <po-button onclick="sendDataToReactNativeApp()"
+              p-label="Teste no index"
+              p-disabled="false"
+            >
+            </po-button>
+          </div>
+          <style>
+          .po-button{
+            width: 200px;
+          }
+          </style>
+          <script>
+          const sendDataToReactNativeApp = () => {
+            window.alert(window);
+            console.log('entendendo o que vem no window: ', window);
+          };
+          </script>
+        </body>
+      </html>`,
+      }}
     />
   );
 }
