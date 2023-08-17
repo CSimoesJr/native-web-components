38a1f0e327c749bf412305914040f4819fc6d129
CSimoesJr <ju_csimoes@hotmail.com>
2023-08-17 17:33:54 -0300

    feat(button): ajusta webview com botão

--- App.tsx
+++ App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useRef, Component, useState, useEffect} from 'react';
+import {Text, Button, View, Alert, SafeAreaView} from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function App() {
   // return <RenderHtml contentWidth={width} source={source} />;
+  let labelButton = 'Label do botão';
+  let textContent!: string;
+
+  const webViewRef = useRef(null)as any;
+
+  const sendPropsToWebView = (props: any) => {
+    if (webViewRef.current) {
+      webViewRef.current.postMessage(JSON.stringify(props));
+    }
+  };
+
+  function onMessage(data: any) {
+    if (data?.nativeEvent?.data) {
+      textContent = data?.nativeEvent?.data;
+    } else {
+      Alert.alert(textContent);
+    }
+  }
+
   return (
     // <WebView
     //   originWhitelist={['*']}
@@ -15,21 +35,36 @@ export default function App() {
     // <WebView
     //   source={{uri: 'http://localhost:3000/'}}
     //   style={{flex: 1}}
-    // />
-    <WebView
-      source={{
-        html: ` 
-        <html lang="en">
+
+    <SafeAreaView style={{flex: 1}}>
+      <View>
+        <Button
+          onPress={onMessage}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about purple button"
+        />
+      </View>
+
+      <WebView
+        onMessage={onMessage}
+        scalesPageToFit={false}
+        mixedContentMode="compatibility"
+        onLoad={() => sendPropsToWebView({ myProp: 'someValue' })}
+        ref={webViewRef}
+        source={{
+          html: ` 
+          <html lang="en">
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <script src="file:///android_asset/build/angular-element.js"></script>
           <link rel="stylesheet" href="file:///android_asset/build/styles.css" />
         </head>
-        <body>
+        <body style="margin-top: 30px">
           <div id="root"></div>
           <div className="App">
             <po-button onclick="sendDataToReactNativeApp()"
-              p-label="Teste no index"
+              p-label="Label botão"
               p-disabled="false"
             >
             </po-button>
@@ -41,13 +76,19 @@ export default function App() {
           </style>
           <script>
           const sendDataToReactNativeApp = () => {
-            window.alert(window);
             console.log('entendendo o que vem no window: ', window);
+            window.ReactNativeWebView.postMessage('Valor de dentro do WebView');
           };
+
+          window.addEventListener('message', event => {
+            const data = JSON.parse(event.data);
+            // Use os dados recebidos para atualizar o conteúdo dentro da WebView
+          });
           </script>
         </body>
       </html>`,
-      }}
-    />
+        }}
+      />
+    </SafeAreaView>
   );
 }
